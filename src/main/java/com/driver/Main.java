11cf58545575eef7834d0151ac2796b4e3e6173e
ayan <theayan44@gmail.com>
2023-06-10 00:03:54 +0530

    master

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y){
+            return (x * y);
+        }
+
+        public int product(int x, int y, int z){
+            return (x * y * z);
+        }
+
+        public double product(double x, double y){
+            return (x * y);
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(4, 4));
+        System.out.println(p.product(4, 4, 4));
+        System.out.println(p.product(4.0, 4.0));
+    }
 
 }
